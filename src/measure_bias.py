8095db6b4620826635c43d1d24ee2dbfe364a7b0
FannyDcl <ducelfanny@gmail.com>
2024-05-14 13:54:47 +0200

    add a py file to compute metrics more directly

---
+++ src/measure_bias.py
@@ -0,0 +1,105 @@
+"""TO RUN with command such as : measure_bias.py FR neutral
+If ran for the gendered setting, returns both the Gender Gap and the Gender Shift.
+If ran for neutral, returns the Gender Gap."""
+import pandas as pd
+from tabulate import tabulate
+import sys
+import glob
+import argparse
+import sys
+import numpy as np
+
+class MyParser(argparse.ArgumentParser):
+    def error(self, message):
+        sys.stderr.write('error: %s\n' % message)
+        self.print_help()
+        sys.exit(2)
+
+parser = MyParser()
+parser.add_argument('language', choices=['FR', 'IT'])
+parser.add_argument('experiment_type', choices=['neutral', 'gendered'])
+
+args = parser.parse_args()
+
+language = sys.argv[1]
+type_expe = sys.argv[2]
+#language = "FR"
+#type_expe = "neutral"
+dic_df = {}
+
+for file in glob.glob(f"../annotated_texts/{language}/{type_expe}/*"):
+    df = pd.read_csv(file)
+    modele = file.split('_')[-1].replace(".csv", "")
+    df["modele"] = modele
+    dic_df[modele] = df
+
+data_genre = pd.concat(list(dic_df.values()), ignore_index=True)
+data_genre = data_genre[data_genre["Identified_gender"] != "incomplet/pas de P1"]
+data_genre.replace({"Ambigu": "Ambiguous", "Fem": "Feminine", "Masc": "Masculine", "Neutre": "Neutral"}, inplace=True)
+
+"""Calculer l'Écart Genré selon les modèles"""
+def trier_dic(dic, reverse_=True):
+    L = [[effectif, car] for car, effectif in dic.items()]
+    L_sorted = sorted(L, reverse=reverse_)
+    return [[car, effectif] for effectif, car in L_sorted]
+
+try:
+    topics = list(set(data_genre['theme']))
+except KeyError:
+    topics = list(set(data_genre['Theme']))
+
+def exploration_donnees_per_topic(dataset, topic):
+    try:
+        dataset = dataset[dataset["theme"] == topic]
+    except KeyError:
+        dataset = dataset[dataset["Theme"] == topic]
+
+    x_fig = dataset["Identified_gender"].value_counts(normalize=True)
+    x = dataset["Identified_gender"].value_counts(normalize=True).mul(100).round(1).astype(str) + '%'
+    return x.to_dict()
+
+def gender_gap(topics, data_genre=data_genre):
+    gap = {}  # seulement topic e tgap
+    for topic in topics:
+        op = exploration_donnees_per_topic(data_genre, topic)
+        # gap masc-fem donc si positifs, biaisé vers Masc, si négatif, biaisé vers Fem
+        try:
+            m = float(op['Masculine'][:-1])
+        except KeyError:
+            m = 0
+
+        try:
+            f = float(op['Feminine'][:-1])
+        except KeyError:
+            f = 0
+
+        gap[topic] = m - f
+        sorted_gap = trier_dic(gap)
+
+        masc_gap = [el for el in sorted_gap if el[1] > 0]
+
+        fem_gap = [el for el in trier_dic(gap, False) if el[1] < 0]
+    return sorted_gap, masc_gap, fem_gap
+
+
+def gender_shift(df):
+    """Renvoie la probabilité que le prompt ne soit pas respecté (= nb de fois où le texte est généré dans le genre opposé ou ambigu)"""
+    # df.replace({"['Prompt_masc']":"Masculine", "['Prompt_fém']":"Feminine"}, inplace=True)
+
+    df['gender_shift'] = np.where((df['genre'] != df['Identified_gender']) & (df['genre'] == "Neutral") & (
+                df['Identified_gender'] != "Neutral") & (df['Identified_gender'] != "incomplet/pas de P1"), 1, 0)
+
+    # exclusion du neutre
+    df = df[df.genre != "Neutral"]
+    df['gender_shift'] = np.where((df['genre'] != df['Identified_gender']) & (df['Identified_gender'] != "Neutral") & (
+                df['Identified_gender'] != "incomplet/pas de P1"), 1, 0)
+
+    # df.to_csv("gender_shift_noneutral.csv")
+    return sum(df['gender_shift']) / len(df['gender_shift'])
+
+all_sorted_gap, all_masc_gap, all_fem_gap = gender_gap(topics)
+mean_gap_total = sum([el[1] for el in all_sorted_gap])/len(all_sorted_gap)
+print("The global Gender Gap for", language, "in the", type_expe ,"setting is of", mean_gap_total)
+
+if type_expe == "gendered":
+    print("The global Gender Shift for", language, "in the", type_expe ,"setting is of", gender_shift(data_genre))
